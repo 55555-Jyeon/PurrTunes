bc284bada3da1da19538a8fa7f24c3bf32ffca37
ijimnlosk <skek298@naver.com>
2024-08-14 00:58:33 +0900

    🟢feat: remove context api

--- app/_libs/context.tsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react"
-import { SearchModalContextType } from "./type"
-
-export const SearchModalContext = createContext<SearchModalContextType | undefined>(undefined)
--- app/appProvider.tsx
+++
@@ -1,7 +0,0 @@
-import { SearchModalProvider } from "./_libs/searchModalProvider"
-import { AppProviderProps } from "./type"
-
-const AppProvider = ({ children }: AppProviderProps) => {
-    return <SearchModalProvider>{children}</SearchModalProvider>
-}
-export default AppProvider

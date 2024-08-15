0a6527ae32f1e8ad1e862a8c425e893a02c37168
55555-Jyeon <jyeon380516@gmail.com>
2024-08-15 14:57:27 +0900

    🎁 style: complete publishing  footer

--- app/_components/layout/footer.tsx
+++ app/_components/layout/footer.tsx
@@ -1,4 +1,8 @@
 const Footer = () => {
-    return <div></div>
+    return (
+        <div className="absolute bottom-0 w-full h-fit flex-center py-2 text-[10px]">
+            2024 &copy; all rights reserved to Amy & Gang
+        </div>
+    )
 }
 export default Footer
--- app/_components/layout/layout.tsx
+++ app/_components/layout/layout.tsx
@@ -3,7 +3,7 @@ import Header from "./header"
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="w-full max-w-[1920px] h-full flex flex-col items-center">
+        <div className="w-full max-w-[1920px] h-full flex-items flex-col">
             <Header />
             <main className="flex-1 mt-20">{children}</main>
             <Footer />

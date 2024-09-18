24a16a0b475ca4b43e395b294585ce9ecf76e5a8
ijimnlosk <skek298@naver.com>
2024-09-18 23:16:53 +0900

    💡refactor: Cat position and speed control

--- app/_/components/layout/catLayout.tsx
+++ app/_/components/layout/catLayout.tsx
@@ -1,10 +1,12 @@
+"use client"
+
 import Image from "next/image"
-import { useCallback, useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 const CatLayout = () => {
     const [position, setPosition] = useState<number>(50)
     const directionRef = useRef(1)
-    const speedRef = useRef(0.5)
+    const speedRef = useRef(0.2)
 
     useEffect(() => {
         const moveInterval = setInterval(() => {
@@ -13,7 +15,7 @@ const CatLayout = () => {
 
                 if (newPosition > 95 || newPosition < 0) {
                     directionRef.current *= -1
-                    speedRef.current = Math.random() * 0.5 + 0.2
+                    speedRef.current = Math.random() * 0.3 + 0.1
                     return prevPosition
                 }
                 return newPosition
@@ -24,18 +26,20 @@ const CatLayout = () => {
     }, [])
 
     return (
-        <div className="relative w-[2480px] h-40 overflow-hidden">
-            <div
-                className="absolute bottom-0 transition-all duration-[50ms] ease-linear"
-                style={{ left: `${position}%` }}
-            >
-                <div className="w-[200px] h-[120px]">
-                    <Image
-                        src={"/right_working_cat.gif"}
-                        alt="cat"
-                        fill
-                        className={`text-gray-700 transition-transform duration-300 ${directionRef.current > 0 ? "scale-x-1" : "scale-x-[-1]"}`}
-                    />
+        <div className="fixed bottom-0 left-0 w-full h-40  overflow-hidden z-50">
+            <div className="relative w-full h-full">
+                <div
+                    className="absolute bottom-0 transition-all duration-[50ms] ease-linear"
+                    style={{ left: `${position}%` }}
+                >
+                    <div className="w-[200px] h-[120px]">
+                        <Image
+                            src={"/right_working_cat.gif"}
+                            alt="cat"
+                            fill
+                            className={`text-gray-700 transition-transform duration-300 ${directionRef.current > 0 ? "scale-x-1" : "scale-x-[-1]"}`}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
